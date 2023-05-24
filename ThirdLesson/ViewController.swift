63eb0555d72b8586c3dbd4eb62d0866a164a3197
AkiraRei <Akirarei@yandex.ru>
2023-05-24 16:01:29 +0300

    newTokken

--- ThirdLesson/ViewController.swift
+++ ThirdLesson/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IBOutlets setup
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMainLabel()
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
     }
-
-
+    // MARK: IBActions
+    @IBAction func segmentedControlAction() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "First segment"
+            mainLabel.textColor = .blue
+        case 1:
+            mainLabel.text = "Seccond segment"
+            mainLabel.textColor = .gray
+        default:
+            mainLabel.text = "Third segment"
+            mainLabel.textColor = .red
+        }
+    }
+    
+    
+    // MARK: private methods
+    private func setupMainLabel() {
+        mainLabel.text = ""
+        mainLabel.font = UIFont.systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 3
+    }
 }
 
